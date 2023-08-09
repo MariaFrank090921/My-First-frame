7ee880e6be6450ef726c81c58d923c65c56d6f7a
MariaFrank090921 <141876510+MariaFrank090921@users.noreply.github.com>
2023-08-09 23:06:42 +0200

    Add files via upload

---
+++ FirstFrameApp.java
@@ -0,0 +1,16 @@
+
+package first.frame.app;
+
+import za.ac.tut.ui.FirstFrameLibrary;
+
+
+public class FirstFrameApp {
+
+  
+    public static void main(String[] args) {
+     
+        FirstFrameLibrary FFA = new  FirstFrameLibrary();
+       
+    }
+    
+}
---
+++ FirstFrameLibrary.java
@@ -0,0 +1,32 @@
+
+package za.ac.tut.ui;
+
+import java.awt.Color;
+import javax.swing.JFrame;
+
+
+public class FirstFrameLibrary  extends JFrame{
+    
+   public FirstFrameLibrary()
+   {
+       
+       setTitle("Diamond Frame");
+       setSize(200,250);
+       setDefaultLookAndFeelDecorated(true);
+       setResizable(true);
+       setDefaultCloseOperation(EXIT_ON_CLOSE);
+       setForeground(Color.BLUE);
+       setVisible(true);
+       
+       
+       
+       
+   
+   
+   
+   
+   }
+   
+    
+    
+}
